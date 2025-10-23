01a24c854eec77c2f54a0ee30adfd26bad7e0abe
John Voronov <john@ops.go>
2025-10-23 23:12:16 +0300

    add changes to the main.go file

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// first comment
+// second comment
+// third comment
+
+func main() {
+	fmt.Println("first message")
+	fmt.Println("second message")
+	fmt.Println("third message")
+}
